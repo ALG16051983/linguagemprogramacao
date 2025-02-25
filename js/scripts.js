37b44b45576578bb54ce3a3f5a275489a2559204
ALG16051983 <andregimenezcx@gmail.com>
2025-02-25 09:05:34 -0300

    Exercicio 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 function seguir(){
     alert('Voce agora esta seguindo Andre Gimenez')
 }
+//exercicio 1
+function exe1(){
+    // vamos usar dom para recuperar os dados do HTML
+    // DOM - Documento Object Model
+    //Converter texto para numero - Number()
+    let nro1 =Number( document.getElementById("nro1").value)
+    let nro2 =Number( document.getElementById("nro2").value)
+    // Fazer a subtracao
+    let sub = nro1 - nro2
+    // vamos mostrar o resultado ao usuario
+    //alert("A subtracao é " + sub)
+    document.getElementById("sub").innerText = "O resultado é " + sub
+}
